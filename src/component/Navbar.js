1eb2bd1703ad28ff44eff95053c2cfc13f982cd2
Murodbek <bekbruno99@gmail.com>
2021-02-09 15:12:20 +0500

    updated

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import Badge from '@material-ui/core/Badge'
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import {NavLink} from 'react-router-dom'
 
 export  function Navbar() {
+    const [check, setCheck] = useState(true)
     return (
         <div>
             <div className="navbar">
             <div className="navbar-part">
-               <span className="navbar-part-info">
-                    <a href="#">Online Market</a>
-                    <ul>
+               <span className={check ? "navbar-part-info checkNavbarFalse": "navbar-part-info checkNavbarTrue" } >
+                    <span className="nav-br-btn">
+                        <a href="#">Online Market</a>
+                        <button 
+                            className="nav-btn"
+                            onClick={() =>{setCheck(!check)}}
+                        >
+                            {check ? <i className="fa fa-2x fa-bars"></i>:<i className="fa fa-2x fa-times"></i>}
+                            
+                        </button>
+                    </span>
+                    <ul className="nav-list">
                         <li><NavLink to="/">Asosiy</NavLink></li>
                         <li><NavLink to="/about">Biz haqimizda</NavLink></li>
                         <li><NavLink to="/connect">Aloqa</NavLink></li>
